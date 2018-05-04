646188d74b1662620a79076f05b21e3cff3946cf
Wangpeihu1 <w18817392281@163.com>
2018-05-04 22:50:39 +0800

    创建网页
    创建主页熟悉django.urls中path用法,映射url

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -1,7 +1,12 @@
 #定义learning_logs的URL模式
-from django.conf.urls import url
+from django.urls import path, re_path
 from . import views
+app_name = 'learning_logs'
 urlpatterns = [
 	#主页
-	url(r'^$',views.index,name='index'),
+	path('',views.index,name='index'),
+	#显示所有主题
+	path('topics/',views.topics,name='topics'),
+	#显示单个主题
+	re_path('topics/(?P<topic_id>\d+)/',views.topic,name='topic')
 ]
