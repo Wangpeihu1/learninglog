6f8933ae7030587fab69b1f493a0782aab974996
Wangpeihu1 <w18817392281@163.com>
2018-05-05 15:24:40 +0800

    创建用户应用
    创建应用
    登陆页面
    注销页面

---
+++ users/urls.py
@@ -0,0 +1,11 @@
+#为应用程序users定义URL模式
+from django.urls import path, re_path
+from django.contrib.auth.views import login
+
+from . import views
+app_name = 'users'
+urlpatterns = [
+	#登陆页面
+	path('login/',login,{'template_name':'users/login.html'},name='login'),
+	path('logout/',views.logout_view,name='logout'),
+]
